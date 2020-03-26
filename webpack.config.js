aabd07f278a3ef2efc668ac21252c2cf0b9636dd
suxiqiang <592364224@qq.com>
2020-03-26 10:24:33 +0800

    feat: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,46 @@
 const path = require("path");
+const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+
+const dirList = [
+  "./src/fundamentals-demo",
+  "./src/other-example"
+];
+
+const entryObj ={
+  index:"./src/index.js"
+};
+const pluginsList = [
+  new CopyPlugin([{
+    from:"./static/**/*"
+  }]),
+  new HtmlWebpackPlugin({
+    filename: "index.html",
+    template: "./src/index.html",
+    chunks: ["index"]
+  })
+];
+
+// traverse all directory , each directory should include at least two file: index.html,index.js.
+dirList.forEach(dir=>{
+  fs.readdirSync(dir).forEach(file => {
+    entryObj[file] = `${dir}/${file}/index.js`;
+    pluginsList.push(
+      new HtmlWebpackPlugin({
+        filename: `${file}.html`,
+        template: `${dir}/${file}/index.html`,
+        chunks: [file]
+      })
+    );
+  });
+});
+
 module.exports = {
-  entry: {
-    index:"./src/index.js",
-    "1-model":"./src/fundamentals-demo/1-model/index.js",
-    "points-flow":"./src/other-example/points-flow/index.js"
-  },
+  entry: entryObj,
   output: {
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new CopyPlugin([{
-      from:"./static/**/*"
-    }]),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-      chunks: ["index"]
-    }),
-    new HtmlWebpackPlugin({
-      filename: "1-model.html",
-      template: "./src/fundamentals-demo/1-model/index.html",
-      chunks: ["1-model"]
-    }),
-    new HtmlWebpackPlugin({
-      filename: "points-flow.html",
-      template: "./src/other-example/points-flow/index.html",
-      chunks: ["points-flow"]
-    })
-  ]
+  plugins:pluginsList
+
 };
