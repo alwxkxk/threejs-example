d1b3474a0fe79efa8049d6775e2becc3cfc51b2c
alwxkxk <alwxkxk@gmail.com>
2020-05-02 10:35:16 +0800

    feat: a模型拆解 界面，功能 优化。

---
+++ src/echart-demo/line.js
@@ -0,0 +1,138 @@
+import * as echarts from "echarts";
+// https://gallery.echartsjs.com/editor.html?c=xBR0Q2QDiH
+
+var myChart = echarts.init(document.getElementById("line"));
+
+var charts = {
+  unit: "Kbps",
+  names: ["出口", "入口"],
+  lineX: ["2018-11-11 17:01", "2018-11-11 17:02", "2018-11-11 17:03", "2018-11-11 17:04", "2018-11-11 17:05", "2018-11-11 17:06", "2018-11-11 17:07", "2018-11-11 17:08", "2018-11-11 17:09", "2018-11-11 17:10", "2018-11-11 17:11", "2018-11-11 17:12", "2018-11-11 17:13", "2018-11-11 17:14", "2018-11-11 17:15", "2018-11-11 17:16", "2018-11-11 17:17", "2018-11-11 17:18", "2018-11-11 17:19", "2018-11-11 17:20"],
+  value: [
+      [451, 352, 303, 534, 95, 236, 217, 328, 159, 151, 231, 192, 453, 524, 165, 236, 527, 328, 129, 530],
+      [360, 545, 80, 192, 330, 580, 192, 80, 250, 453, 352, 28, 625, 345, 65, 325, 468, 108, 253, 98]
+  ]
+
+};
+var color = ["rgba(23, 255, 243", "rgba(255,100,97"];
+var lineY = [];
+
+for (var i = 0; i < charts.names.length; i++) {
+  var x = i;
+  if (x > color.length - 1) {
+      x = color.length - 1;
+  }
+  var data = {
+      name: charts.names[i],
+      type: "line",
+      color: color[x] + ")",
+      smooth: true,
+      areaStyle: {
+          normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                  offset: 0,
+                  color: color[x] + ", 0.3)"
+              }, {
+                  offset: 0.8,
+                  color: color[x] + ", 0)"
+              }], false),
+              shadowColor: "rgba(0, 0, 0, 0.1)",
+              shadowBlur: 10
+          }
+      },
+      symbol: "circle",
+      symbolSize: 5,
+      data: charts.value[i]
+  };
+  lineY.push(data);
+}
+
+lineY[0].markLine = {
+  silent: true,
+  data: [{
+      yAxis: 5
+  }, {
+      yAxis: 100
+  }, {
+      yAxis: 200
+  }, {
+      yAxis: 300
+  }, {
+      yAxis: 400
+  }]
+};
+var option = {
+  backgroundColor:"rgba(0,0,0,0.4)",
+  tooltip: {
+      trigger: "axis"
+  },
+  legend: {
+      data: charts.names,
+      textStyle: {
+          fontSize: 12,
+          color: "rgb(0,253,255,0.6)"
+      },
+      right: "4%"
+  },
+  grid: {
+      top: "14%",
+      left: "4%",
+      right: "4%",
+      bottom: "12%",
+      containLabel: true
+  },
+  xAxis: {
+      type: "category",
+      boundaryGap: false,
+      data: charts.lineX,
+      axisLabel: {
+          textStyle: {
+              color: "rgb(0,253,255,0.6)"
+          },
+          formatter: function(params) {
+              return params.split(" ")[0] + "\n" + params.split(" ")[1];
+          }
+      }
+  },
+  yAxis: {
+      name: charts.unit,
+      type: "value",
+      axisLabel: {
+          formatter: "{value}",
+          textStyle: {
+              color: "rgb(0,253,255,0.6)"
+          }
+      },
+      splitLine: {
+          lineStyle: {
+              color: "rgb(23,255,243,0.3)"
+          }
+      },
+      axisLine: {
+          lineStyle: {
+              color: "rgb(0,253,255,0.6)"
+          }
+      }
+  },
+  series: lineY
+};
+setInterval(() => {
+  myChart.setOption({
+    legend: {
+      selected: {
+        "出口": false,
+        "入口": false
+      }
+    }
+  });
+  myChart.setOption({
+    legend: {
+      selected: {
+        "出口": true,
+        "入口": true
+      }
+    }
+  });
+},10000);
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
--- src/project-demo/a-dismantling/index.js
+++ src/project-demo/a-dismantling/index.js
@@ -1,21 +1,49 @@
+// 引入 echart 示例代码
+// import "../../echart-demo/line.js";
+
 import * as THREE from "three";
 import TWEEN from "@tweenjs/tween.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-const scene = new THREE.Scene();
-// 修改成灰色背景
-scene.background = new THREE.Color( 0xaaaaaa );
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.x = -7;
-camera.position.z = 7;
-camera.position.y = 7;
+// outline postprocessing
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
+import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader.js";
 
-const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const loader = new GLTFLoader();
+const itemList = []; // 存放raycaster检测对象
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.set(-7,7,7);
+
+const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 new OrbitControls( camera, renderer.domElement );
+
+// outline 
+const composer = new EffectComposer( renderer );
+
+const renderPass = new RenderPass( scene, camera );
+composer.addPass( renderPass );
+
+const outlinePass = new OutlinePass( new THREE.Vector2( window.innerWidth, window.innerHeight ), scene, camera );
+outlinePass.edgeStrength = 5;
+outlinePass.edgeGlow = 1;
+outlinePass.pulsePeriod = 2;
+outlinePass.visibleEdgeColor.set("#35f2d1");
+outlinePass.hiddenEdgeColor.set("#00ffff");
+composer.addPass( outlinePass );
+
+const effectFXAA = new ShaderPass( FXAAShader );
+effectFXAA.uniforms[ "resolution" ].value.set( 1 / window.innerWidth, 1 / window.innerHeight );
+composer.addPass( effectFXAA );
+
 // 添加光源
 const light = new THREE.HemisphereLight( 0xffffff,0xcccccc ); // soft white light
 scene.add( light );
@@ -35,23 +63,6 @@ spotLight3.position.set( -6, 9, -1 );
 spotLight3.angle =1.3;
 scene.add( spotLight3 );
 
-const animate = function () {
-  requestAnimationFrame( animate );
-  TWEEN.update();
-  renderer.render( scene, camera );
-};
-
-
-
-const loader = new GLTFLoader();
-loader.load( "./static/3d/a-dismantling.glb", function ( gltf ) {
-
-  scene.add( gltf.scene );
-  console.log("scene",scene);
-
-  animate();
-
-} );
 
 // 移动函数
 function move(obj,position) {
@@ -99,6 +110,69 @@ document.getElementById("recovery").onclick=()=>{
   move(scene.getObjectByName("Object_28"),{y:0});
 };
 
+
+// 选中高亮并显示名称
+//https://github.com/mrdoob/three.js/blob/master/examples/webgl_instancing_raycast.html
+// https://threejs.org/examples/?q=outline#webgl_postprocessing_outline
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2( 1, 1 );
+const mousePosition = {
+  x:0,
+  y:0
+};
+
+function onMouseMove( event ) {
+
+  event.preventDefault();
+  mousePosition.x =event.clientX;
+  mousePosition.y =event.clientY;
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+document.addEventListener( "mousemove", onMouseMove, false );
+const nameBox = document.getElementById("name-box");
+const animate = function () {
+  TWEEN.update();
+  raycaster.setFromCamera(mouse, camera);
+  const intersection = raycaster.intersectObjects( itemList );
+  if(intersection.length>0){
+    // console.log("intersection",intersection);
+    // 给选中的物体 设置outline效果
+    outlinePass.selectedObjects = [intersection[0].object];
+
+    // 名称提示
+    nameBox.innerHTML = intersection[0].object.name.replace("Object_","元件");
+    nameBox.style.display = "block";
+    nameBox.style.top = mousePosition.y;
+    nameBox.style.left = mousePosition.x + 30;
+  }else{
+    outlinePass.selectedObjects = [];
+    nameBox.style.display = "none";
+  }
+  composer.render();
+  requestAnimationFrame( animate );
+};
+
+
+
+
+
+loader.load( "./static/3d/a-dismantling.glb", function ( gltf ) {
+   
+  scene.add( gltf.scene.children[0] );
+
+  console.log("scene",scene);
+  scene.traverse(item=>{
+    itemList.push(item);
+  });
+  
+
+  animate();
+
+} );
 
 
 
