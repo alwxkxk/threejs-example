cfe8e399c2b686a892687a4a05f082b03a97e4aa
suxiqiang <592364224@qq.com>
2020-05-19 19:04:34 +0800

    feat: 3 光迹 添加bloom效果。

--- src/fundamentals-demo/3-light-trail/index.js
+++ src/fundamentals-demo/3-light-trail/index.js
@@ -3,22 +3,36 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import {interpolateHsl} from "d3-interpolate";
 import TWEEN from "@tweenjs/tween.js";
 
+// bloom
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+
 const scene = window.debugScene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+// NOTE：camera 坐标不能全为0，否则OrbitControls异常，无法移动视觉
+camera.position.x = 500;
+camera.position.z = 100;
+camera.position.y = 50;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
 new OrbitControls( camera, renderer.domElement );
 const light = new THREE.HemisphereLight( 0xffffff, 0xcccccc, 1 );
 scene.add( light );
-// NOTE：camera 坐标不能全为0，否则OrbitControls异常，无法移动视觉
-camera.position.z = 200;
-camera.position.y = 30;
 
-
+// bloom效果
+const renderScene = new RenderPass( scene, camera );
+const bloomPass  = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+bloomPass .threshold = 0.3;
+bloomPass .strength = 2;
+bloomPass .radius = 0.3;
+const bloomComposer = new EffectComposer( renderer );
+bloomComposer.addPass( renderScene );
+bloomComposer.addPass( bloomPass  );
 
 // 生成曲线
 const startVec = new THREE.Vector3(500,0,0);
@@ -26,9 +40,9 @@ const controlVec = new THREE.Vector3(-400,0,20);
 const endVec = new THREE.Vector3(-500,0,300);
 const curve = new THREE.QuadraticBezierCurve3(startVec,controlVec,endVec);
 
-var geometry = new THREE.TubeBufferGeometry( curve, 20, 2, 8, false );
+const geometry = new THREE.TubeBufferGeometry( curve, 20, 2, 8, false );
 // 使用顶点颜色 VertexColors
-var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+const material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors,side: THREE.BackSide } );
 
 // 生成渐变色的color数组
 const count = geometry.attributes.position.count;
@@ -38,7 +52,7 @@ for (let index = 0; index < count; index++) {
   const t = index / count;
   const rgb = rgbInterpolate(t);
   const rgbValue = rgb.match(/\d+/g);
-
+  // 从 "rgb(1,2,3)" 字符串里 提取出 1,2,3 并 归一化（ 0.0 ~ 1.0）
   const r = Number(rgbValue[0]) / 255;
   const g = Number(rgbValue[1]) / 255;
   const b = Number(rgbValue[2]) / 255;
@@ -47,7 +61,7 @@ for (let index = 0; index < count; index++) {
   colorArray[3 * index + 1] = g;  
   colorArray[3 * index + 2] =  b;
 }
-geometry.addAttribute( "color", new THREE.Float32BufferAttribute( colorArray, 3 ) );
+geometry.setAttribute( "color", new THREE.Float32BufferAttribute( colorArray, 3 ) );
 
 const mesh = new THREE.Mesh( geometry, material);
 scene.add( mesh );
@@ -62,18 +76,19 @@ function lightMove() {
     const b = colorArray.slice(anchor * 3);
     const f = colorArray.slice(0, anchor * 3);
     const newColorArray = [].concat(b, f) ;
-    mesh.geometry.addAttribute( "color", new THREE.Float32BufferAttribute( newColorArray, 3 ) );
+    mesh.geometry.setAttribute( "color", new THREE.Float32BufferAttribute( newColorArray, 3 ) );
   })
   .start();
 }
 
 setInterval(() => {
   lightMove();
-}, 1200);
+}, 1000);
 
 const animate = function () {
   TWEEN.update();
-  renderer.render( scene, camera );
+  // renderer.render( scene, camera );
+  bloomComposer.render();
   requestAnimationFrame( animate );
 
 };
